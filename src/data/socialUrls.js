66d2075d22ac1e2556ecd4302d4cdf21cfd0d82b
alrwest <alrwest@iu.edu>
2026-09-10 00:41:53 -0400

    added resume pdf

--- src/data/socialUrls.js
+++ src/data/socialUrls.js
@@ -1,7 +1,7 @@
 const SOCIAL_URLS = {
   github: "https://github.com/1want2sayhello",
   linkedIn: "https://www.linkedin.com/in/alrwest/",
-  resume: "/resume.pdf",
+  resume: "/Allyson-West-Resume.pdf",
 };
 
 export default SOCIAL_URLS;
